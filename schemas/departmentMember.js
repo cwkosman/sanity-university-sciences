df4a268aa0373fd6d3fe44555b0a6cf0b2b231d3
Chuck Kosman <cwkosman@gmail.com>
2022-07-06 16:37:18 -0700

    Department member content model

---
+++ schemas/departmentMember.js
@@ -0,0 +1,75 @@
+import {defineType, defineField} from 'sanity'
+
+export const departmentMember = defineType({
+  name: 'departmentMember',
+  type: 'document',
+  title: 'Department Member',
+  fields: [
+    defineField({
+      name: 'firstName',
+      type: 'string',
+      title: 'First Name',
+    }),
+    defineField({
+      name: 'lastName',
+      type: 'string',
+      title: 'Last Name',
+    }),
+    defineField({
+      name: 'academicRank',
+      type: 'string',
+      title: 'Academic Rank',
+      options: {
+        list: ['Professor Emeritus', 'Professor', 'Associate Professor', 'Assistant Professor'],
+      },
+      readOnly: ({currentUser}) => {
+        return !currentUser.roles.find((role) => role.name === 'administrator')
+      },
+    }),
+    defineField({
+      name: 'photo',
+      type: 'image',
+      title: 'Photo',
+      fields: [
+        {
+          name: 'attribution',
+          type: 'string',
+          title: 'Atrribution',
+        },
+      ],
+      options: {
+        hotspot: true,
+      },
+    }),
+    defineField({
+      name: 'officeNumber',
+      type: 'string',
+      title: 'Office Number',
+      validation: function (rule) {
+        return rule
+          .regex(/^([A-Z]){0,3} (\d){1,4}$/)
+          .error(
+            'Use up to 3 letter building code followed by up to 4 digit room number. For example, "ESB 3104"'
+          )
+      },
+    }),
+    defineField({
+      name: 'phoneNumber',
+      type: 'string',
+      title: 'Phone Number',
+      validation: function (rule) {
+        return rule
+          .regex(/^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s./0-9]*$/)
+          .error('Use a valid 9 or 10 digit phone number')
+      },
+    }),
+    defineField({
+      name: 'acceptingStudents',
+      type: 'boolean',
+      title: 'Accepting Students',
+      // options: {
+      //   layout: 'checkbox',
+      // },
+    }),
+  ],
+})
--- schemas/index.ts
+++ schemas/index.ts
@@ -1,1 +1,3 @@
-export const schemaTypes = []
+import {departmentMember} from './departmentMember'
+
+export const schemaTypes = [departmentMember]
